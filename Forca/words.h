897d9ac66213a871c13feda87c8519bbae302b02
littlesekii <littlesekii@gmail.com>
2024-01-03 21:31:44 -0300

    Listas de palavras alteradas para maiúsculo; Adicionado novo campo em palavras, para sanitizar a acentuação.

--- Forca/words.h
+++ Forca/words.h
@@ -1,12 +1,13 @@
 namespace Word {
 
-    struct Word {
-        char word[15];
-        char theme[30];
-        bool wasAlreadyPlayed{};
-    };
+	struct Word {
+		char word[15];
+		char cleanWord[15];
+		char theme[30];
+		bool wasAlreadyPlayed{};
+	};
 
-    extern Word PLAYABLE_WORDS[50];
+    extern Word PLAYABLE_WORDS[];
 
     Word randomWordFromList(Word* words);
 }
